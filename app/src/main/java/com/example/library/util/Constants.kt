4066a6716f4445aa46a5dc8ca7de29c579458fe3
Oscar Ivan Ramirez <oscivaramtin@gmail.com>
2020-12-01 21:54:18 -0500

    AS-31
    AS-31(Update book gender): Update gender feature was included, the message for successfully or error in the process is showing through a banner.

--- app/src/main/java/com/example/library/util/Constants.kt
+++ app/src/main/java/com/example/library/util/Constants.kt
@@ -7,4 +7,15 @@ object Constants {
     const val COLLECTION_OWNER = "COLLECTION_OWNER"
 
     const val DB_NAME = "BOOKS_DB"
+
+    //region touchListener threshold
+    const val SWIPE_THRESHOLD = 100
+    const val SWIPE_VELOCITY_THRESHOLD = 100
+    //endregion
+
+    //region error on CRUD operation
+    const val ERROR_TRYING_TO_PERFORM_UPDATE = "Error trying to update"
+
+    //endregion
+
 }
--- app/src/main/java/com/example/library/util/LivedataHelper.kt
+++ app/src/main/java/com/example/library/util/LivedataHelper.kt
@@ -1,19 +1,22 @@
 package com.example.library.util
 
+import android.content.Context
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import com.example.library.BaseActivity
 import com.example.library.states.State
+import com.example.library.util.Constants.ERROR_TRYING_TO_PERFORM_UPDATE
 import kotlinx.android.synthetic.main.activity_auth_layout.*
 import kotlinx.android.synthetic.main.activity_main.*
 
-fun <T> Fragment.observe(liveData: LiveData<State<T>>, success: (T) -> Unit){
+fun <T> Fragment.observe(liveData: LiveData<State<T>>, success: (T) -> Unit) {
 
     liveData.observe(this.viewLifecycleOwner, Observer { state ->
-        when(state){
+        when (state) {
             is State.Loading -> {
                 Log.i("subscribeObservers", "Loading: $state")
                 this.activity?.progress_circular?.visibility = View.VISIBLE
@@ -28,15 +31,22 @@ fun <T> Fragment.observe(liveData: LiveData<State<T>>, success: (T) -> Unit){
             is State.Failed -> {
                 Log.i("subscribeObservers", "Failed: $state")
                 this.activity?.progress_circular?.visibility = View.GONE
+                if (state.message.isNotEmpty())
+                    this.activity?.let {
+                        showErrorBannerIfConvenient(it, state.message)
+                    }
             }
         }
     })
 }
 
-fun <T> Fragment.observeAndPreventsHandleEventAgain(liveData: LiveData<State<T>>, success: (T) -> Unit){
+fun <T> Fragment.observeAndPreventsHandleEventAgain(
+    liveData: LiveData<State<T>>,
+    success: (T) -> Unit
+) {
 
     liveData.observe(this.viewLifecycleOwner, Observer { state ->
-        when(state){
+        when (state) {
             is State.Loading -> {
                 Log.i("subscribeObservers", "Loading: $state")
                 this.activity?.progress_circular?.visibility = View.VISIBLE
@@ -53,15 +63,19 @@ fun <T> Fragment.observeAndPreventsHandleEventAgain(liveData: LiveData<State<T>>
             is State.Failed -> {
                 Log.i("subscribeObservers", "Failed: $state")
                 this.activity?.progress_circular?.visibility = View.GONE
+                if (state.message.isNotEmpty())
+                    this.activity?.let {
+                        showErrorBannerIfConvenient(it, state.message)
+                    }
             }
         }
     })
 }
 
-fun <T> AppCompatActivity.observe(liveData: LiveData<State<T>>, success: (T) -> Unit){
+fun <T> AppCompatActivity.observe(liveData: LiveData<State<T>>, success: (T) -> Unit) {
 
     liveData.observe(this, Observer { state ->
-        when(state){
+        when (state) {
             is State.Loading -> {
                 Log.i("subscribeObservers", "Loading: $state")
                 progress_circular_auth.visibility = View.VISIBLE
@@ -76,7 +90,19 @@ fun <T> AppCompatActivity.observe(liveData: LiveData<State<T>>, success: (T) -> 
             is State.Failed -> {
                 Log.i("subscribeObservers", "Failed: $state")
                 progress_circular_auth.visibility = View.GONE
+                if (state.message.isNotEmpty())
+                    showErrorBannerIfConvenient(this, state.message)
             }
         }
     })
+}
+
+fun showErrorBannerIfConvenient(context: Context, errorMessage: String) {
+
+    when (errorMessage) {
+        ERROR_TRYING_TO_PERFORM_UPDATE -> (context as BaseActivity).showBanner(
+            errorMessage,
+            BaseActivity.BannerType.ERROR
+        )
+    }
 }
